62ce21bc0dc8c75ab0926ff2412382845992e5c7
JasonWang <ff1986755087@gmail.com>
2020-12-29 00:08:07 +0800

    測試tailwind.config.js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="home">
     <topmenu/>
-    <img alt="Vue logo" class="m-auto" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" class="m-auto" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul class="grid lg:grid-cols-4 md:grid-cols-2 gap-5 container m-auto">
+      <li class="text-mainColor">1</li>
+      <li class="text-mainColor">2</li>
+      <li class="text-mainColor">3</li>
+      <li class="text-mainColor">4</li>
+      <li class="bg-mainColor opacity-3 text-5xl">5</li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +19,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 import topmenu from '@/components/topmenu.vue';
 
 export default {
-  name: 'Home',
+  // name: 'Home',
   components: {
     HelloWorld,
     topmenu,
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors:{
+      mainColor: '#ec4444',
+    },
     extend: {},
   },
   variants: {
