db20789b07eb3e77036e66b8342853db2d9a79a2
JasonWang <ff1986755087@gmail.com>
2020-12-28 15:15:37 +0800

    init v2

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,25 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    // name: 'Home',
     component: Home,
   },
   {
     path: '/about',
-    name: 'About',
+    // name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/content',
+    component: () => import(/* webpackChunkName: "about" */ '../views/content.vue'),
+  },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
---
+++ src/views/content.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content">
+    <h1>This is an about page</h1>
+    <contentmsg msg="Welcome"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import contentmsg from '@/components/contentmsg.vue';
+
+export default {
+  // name: 'content',
+  components: {
+    contentmsg,
+  },
+};
+</script>
