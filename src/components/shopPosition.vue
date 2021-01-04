e3a5993caf740b4300f2718699bdd4ef510e69a4
JasonWang <ff1986755087@gmail.com>
2021-01-05 01:14:06 +0800

    增加json

---
+++ src/components/shopPosition.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="shopPosition">
+  <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100" @change="cityToArea">
+    <option :value="itemt.CityName" v-for="itemt in taiwan" :key="itemt">{{itemt.CityName}}</option>
+  </select>
+  <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100">
+    <option v-for="items in taiwan.AreaList">{{items.AreaName}}</option>
+  </select>
+</div>
+</template>
+
+<script>
+import taiwan from '../../static/resource/CityCountyData.json';
+
+export default {
+  name: 'shopPosition',
+  data() {
+    return {
+      taiwan ,
+    };
+  },
+  mounted() {
+    console.log(this.taiwan);
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <topmenu/>
     <div class="topBanner relative">
       <img src="../assets/drinkCup.png" alt="" srcset="" class="absolute bottom-0">
@@ -31,24 +30,7 @@
     <div class="drinkMagic text-center mt-48">
       <h2 class="text-4xl font-bold">立即體驗</h2>
       <p class="mt-3">搜尋離你最近的PayPayDrink服務據點，開始不一樣的生活</p>
-      <div id="shopPosition">
-        <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100">
-          <option :value="item.name" v-for="item in taiwan" :key="item">{{item.name}}</option>
-        </select>
-        <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100">
-          <option value="area1">中正區</option>
-          <option value="area2">大同區</option>
-          <option value="area3">中山區</option>
-          <option value="area4">萬華區</option>
-          <option value="area5">信義區</option>
-          <option value="area6">松山區</option>
-          <option value="area7">大安區</option>
-          <option value="area8">南港區</option>
-          <option value="area9">北投區</option>
-          <option value="area10">內湖區</option>
-          <option value="area11">士林區</option>
-          <option value="area12">文山區</option>
-        </select>
+        <shopPosition/>
       </div>
     </div>
   </div>
@@ -56,24 +38,19 @@
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue';
 import topmenu from '@/components/topmenu.vue';
-import slide from '@/components/slide.vue';
 import carousel from '@/components/carousel.vue';
-import taiwan from '../../static/resource/taiwan_districts.json';
-// const config = JSON.parse(taiwan);
+import shopPosition from '@/components/shopPosition.vue';
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld,
     topmenu,
-    slide,
     carousel,
+    shopPosition,
   },
   data() {
     return {
-      taiwan ,
       magic: [
         {
           icon: 'pay-mobile',
@@ -113,6 +90,11 @@ export default {
       ],
     };
   },
+  // methods: {
+  //   cityToArea(i) {
+  //     console.log(this.indexOf());
+  //   },
+  // },
 };
 </script>
 
