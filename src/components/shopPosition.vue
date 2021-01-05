a44ac0c2ec1cd402a3ec3c2d20fca11a034e90b6
JasonWang <ff1986755087@gmail.com>
2021-01-06 01:01:31 +0800

    完成shopList&down

--- src/components/shopPosition.vue
+++ src/components/shopPosition.vue
@@ -1,11 +1,62 @@
 <template>
-  <div id="shopPosition">
-    <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100" @change="cityToArea">
-      <option :value="itemt.CityName" v-for="(itemt, index) in taiwan" :key="index" :data="index">{{index}}{{itemt.CityName}}</option>
-    </select>
-    <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100">
-      <option v-for="(items, index) of taiwan[0].AreaList" :key="index">{{items.AreaName}}</option>
-    </select>
+  <div id="shopPosition" class="mt-5">
+    <form action="" method="post">
+      <select
+        name=""
+        id=""
+        class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100 w-80 mr-5"
+        @change="cityToArea"
+      >
+        <option :value="itemt.CityName" v-for="(itemt, index) in taiwan" :key="index" :data="index">
+          {{ index }}{{ itemt.CityName }}
+        </option>
+      </select>
+      <select
+        name=""
+        id=""
+        class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100 w-80"
+      >
+        <option v-for="(items, index) of taiwan[0].AreaList" :key="index">
+          {{ items.AreaName }}
+        </option>
+      </select>
+    </form>
+    <ul class="shopResult mx-auto my-10 max-w-4xl">
+      <li
+        v-for="(shop, index) in shopList"
+        :key="index"
+        class="flex items-center justify-between border-b border-support3-100 mb-5"
+      >
+        <div class="shopInfo text-left text-mainColor-100">
+          <div class="shopName text-2xl font-bold">
+            {{ shop.name }}
+          </div>
+          <ul class="flex mt-2 pb-5">
+            <li>
+              <span class="pay-pin"></span>
+              {{ shop.postition }}
+            </li>
+            <li>
+              <span class="pay-tel"></span>
+              {{ shop.tel }}
+            </li>
+            <li>
+              <span class="pay-clock"></span>
+              {{ shop.openTime }}
+            </li>
+          </ul>
+        </div>
+        <div class="shopLink">
+          <a
+            :href="shop.mapLink"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="hover:text-mainColor-100 hover:border-mainColor-100 duration-200 text-support1-100 border py-2 px-4 border-support1-100 rounded-md"
+            >查看位置</a
+          >
+        </div>
+      </li>
+    </ul>
     <!-- <select name="" id="" class="border px-4 py-2 border-support3-100 rounded-md text-mainColor-100">
       <option v-for="(items, index) of info.data.retVal" :key="index">{{items.sna}}</option>
     </select> -->
@@ -13,32 +64,65 @@
 </template>
 
 <script>
-import taiwan from '../../static/resource/CityCountyData.json';
+  import taiwan from '../../static/resource/CityCountyData'
 
-export default {
-  name: 'shopPosition',
-  data() {
-    return {
-      taiwan ,
-      // info: null,
-    };
-  },
-  methods :{ //方法
-    cityToArea() {
-      var city = event.target.data;
-      console.log(city);
+  export default {
+    name: 'shopPosition',
+    data() {
+      return {
+        taiwan,
+        // info: null,
+        shopList: [
+          {
+            name: '配配飲 一號店',
+            postition: '台北市大安區仁愛路四段345巷6弄78號',
+            tel: '02-2456-7891',
+            openTime: '24H 全年無休',
+            mapLink: 'http://google.com.tw'
+          },
+          {
+            name: '配配飲 一號店',
+            postition: '台北市大安區仁愛路四段345巷6弄78號',
+            tel: '02-2456-7891',
+            openTime: '24H 全年無休',
+            mapLink: 'http://google.com.tw'
+          },
+          {
+            name: '配配飲 一號店',
+            postition: '台北市大安區仁愛路四段345巷6弄78號',
+            tel: '02-2456-7891',
+            openTime: '24H 全年無休',
+            mapLink: 'http://google.com.tw'
+          }
+        ]
+      }
     },
-  },
-  mounted: { //架好的
-    // axios
-    //   .get('https://api.kcg.gov.tw/api/service/Get/b4dd9c40-9027-4125-8666-06bef1756092')
-    //   .then(response => (this.info = response.data))
-    //   .catch(function(error) { // 請求失敗處理
-    //         console.log(error);
-    // });
-  },
-};
+    methods: {
+      //方法
+      cityToArea() {
+        var city = event.target.data
+        console.log(city)
+      }
+    },
+    mounted: {
+      //架好的
+      // axios
+      //   .get('https://api.kcg.gov.tw/api/service/Get/b4dd9c40-9027-4125-8666-06bef1756092')
+      //   .then(response => (this.info = response.data))
+      //   .catch(function(error) { // 請求失敗處理
+      //         console.log(error);
+      // });
+    }
+  }
 </script>
 <style lang="scss" scoped>
-
+  .shopInfo li {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+    span {
+      color: #f39519;
+      margin-right: 10px;
+    }
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,106 +1,117 @@
 <template>
   <div class="home">
-    <topmenu/>
+    <topmenu />
     <div class="topBanner relative">
-      <img src="../assets/drinkCup.png" alt="" srcset="" class="absolute bottom-0">
-      <img src="../assets/topBannerBg.jpg" alt="" srcset="">
+      <img src="../assets/drinkCup.png" alt="" srcset="" class="absolute bottom-0" />
+      <img src="../assets/topBannerBg.jpg" alt="" srcset="" />
     </div>
     <div class="saleActive container m-auto text-center relative z-0">
       <h2 class="text-4xl font-bold text-white">優惠活動</h2>
       <p class="mt-3 text-subColor-100">快揪好友，省錢趁現在！</p>
       <div class="flex justify-center mt-10">
-        <carousel/>
+        <carousel />
       </div>
     </div>
     <div class="drinkMagic text-center mt-48">
       <h2 class="text-4xl font-bold">配配飲的魔法</h2>
       <p class="mt-3">我們堅持經典也擁抱創新，只是希望能看到您滿足的笑容 :D</p>
-      <ul class="grid gap-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-20">
-        <li class="flex bg-support2-100 p-10 rounded-xl" v-for="item in magic" :key="item">
+      <ul class="grid gap-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 px-20 py-10">
+        <li class="flex bg-support2-100 p-8 rounded-xl" v-for="item in magic" :key="item">
           <div class="icon mr-5 text-subColor-100">
             <span :class="item.icon" class="text-5xl"></span>
           </div>
           <div class="infotext text-left">
-            <h3 class="text-xl font-bold text-mainColor-100">{{item.title}}</h3>
-            <p class="mt-2 text-gray-600">{{item.text}}</p>
+            <h3 class="text-xl font-bold text-mainColor-100">{{ item.title }}</h3>
+            <p class="mt-2 text-gray-600">{{ item.text }}</p>
           </div>
         </li>
       </ul>
     </div>
-    <div class="drinkMagic text-center mt-48">
+    <div class="drinkMagic text-center mt-20">
       <h2 class="text-4xl font-bold">立即體驗</h2>
       <p class="mt-3">搜尋離你最近的PayPayDrink服務據點，開始不一樣的生活</p>
-        <shopPosition/>
+      <shopPosition />
+    </div>
+    <div class="downApp bg-mainColor-100 mt-52">
+      <div class="container m-auto text-center relative py-16">
+        <img src="../assets/downhand.svg" alt="" srcset="" class="absolute bottom-0 left-40" />
+        <h2 class="text-white text-4xl font-extrabold">
+          立即下載<span class="text-subColor-100 ml-3">配配飲</span>
+        </h2>
+        <p class="text-white mt-3 mb-6">客製屬於您的飲品</p>
+        <img src="../assets/appDown.png" alt="" srcset="" class="block m-auto" />
       </div>
     </div>
+    <footerBar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import topmenu from '@/components/topmenu.vue';
-import carousel from '@/components/carousel.vue';
-import shopPosition from '@/components/shopPosition.vue';
+  // @ is an alias to /src
+  import topmenu from '@/components/topmenu'
+  import carousel from '@/components/carousel'
+  import shopPosition from '@/components/shopPosition'
+  import footerBar from '@/components/footerBar'
 
-export default {
-  name: 'Home',
-  components: {
-    topmenu,
-    carousel,
-    shopPosition,
-  },
-  data() {
-    return {
-      magic: [
-        {
-          icon: 'pay-mobile',
-          title:'一機在手，威力無窮',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-Cheers',
-          title:'歡迎光臨，您想喝什麼呢?',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-clock',
-          title:'可以半糖少冰嗎?',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-mobile',
-          title:'還等到天荒地老，海枯石爛嗎?',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-mobile',
-          title:'多元支付，比便利商店還方便！',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-qrcode',
-          title:'自己喝還是請客，掃碼一次搞定',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-        {
-          icon: 'pay-happy',
-          title:'進入口中的瞬間，給您滿足的笑容',
-          text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限',
-        },
-      ],
-    };
-  },
-  // methods: {
-  //   cityToArea(i) {
-  //     console.log(this.indexOf());
-  //   },
-  // },
-};
+  export default {
+    name: 'Home',
+    components: {
+      topmenu,
+      carousel,
+      shopPosition,
+      footerBar
+    },
+    data() {
+      return {
+        magic: [
+          {
+            icon: 'pay-mobile',
+            title: '一機在手，威力無窮',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-Cheers',
+            title: '歡迎光臨，您想喝什麼呢?',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-clock',
+            title: '可以半糖少冰嗎?',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-mobile',
+            title: '還等到天荒地老，海枯石爛嗎?',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-mobile',
+            title: '多元支付，比便利商店還方便！',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-qrcode',
+            title: '自己喝還是請客，掃碼一次搞定',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          },
+          {
+            icon: 'pay-happy',
+            title: '進入口中的瞬間，給您滿足的笑容',
+            text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
+          }
+        ]
+      }
+    }
+    // methods: {
+    //   cityToArea(i) {
+    //     console.log(this.indexOf());
+    //   },
+    // },
+  }
 </script>
 
 <style lang="scss" scoped>
-.saleActive{
-  margin-top: -310px;
-}
-
+  .saleActive {
+    margin-top: -310px;
+  }
 </style>
