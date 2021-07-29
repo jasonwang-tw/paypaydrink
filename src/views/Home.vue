0cf7eb914012c8a7975320442aca7008a22c23a8
Jason Wang <ff1986755087@gmail.com>
2021-07-29 13:59:21 +0800

    變更帳號驗證流程

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,9 @@
   <div id="home">
     <topmenu />
     <div class="topBanner relative">
-      <h1 class="indexTitle lg:text-7xl text-main-500 text-6xl container m-auto xl:py-20 xl:px-60 py-20 px-10">
+      <h1
+        class="indexTitle lg:text-7xl text-main-500 text-6xl container m-auto xl:py-20 xl:px-60 py-20 px-10"
+      >
         <span class="font-light">配配</span><span>飲</span><br /><span
           class="lg:text-5xl text-4xl text-sub-500"
           >您專屬的飲品魔法師</span
@@ -21,14 +23,14 @@
       <div class="magicBg absolute w-full"></div>
       <!-- <img src="../assets/gradientBg.png" alt="" srcset="" class="absolute z-0 center" /> -->
       <div class="relative text-main-500">
-        <img src="../assets/drink1.png" alt="" srcset="" class="mx-auto mb-10">
+        <img src="../assets/drink1.png" alt="" srcset="" class="mx-auto mb-10" />
         <h2 class="">配配飲的魔法</h2>
         <p class="mt-3">我們堅持經典也擁抱創新，只是希望能看到您滿足的笑容 :D</p>
         <ul
           class="magicList grid gap-5 xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 lg:px-20 md:px-10 px-5 py-10"
         >
           <li class="flex p-8 rounded-xl" v-for="item in magic" :key="item">
-            <div class="icon mr-5 text-sub-500">
+            <div class="icon mr-5 text-sub-500 w-1/6 flex-shrink-0">
               <i :class="item.icon" class="text-5xl"></i>
             </div>
             <div class="infotext text-left">
@@ -40,7 +42,7 @@
       </div>
     </div>
     <div class="text-center mt-20 relative text-main-500">
-      <img src="../assets/drink2.png" alt="" srcset="" class="mx-auto mb-10">
+      <img src="../assets/drink2.png" alt="" srcset="" class="mx-auto mb-10" />
       <h2 class="">立即體驗</h2>
       <p class="mt-3">搜尋離你最近的PayPayDrink服務據點，開始不一樣的生活</p>
       <shopPosition />
@@ -88,27 +90,27 @@
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
           {
-            icon: 'pay-Cheers',
+            icon: 'pay-drink',
             title: '歡迎光臨，您想喝什麼呢?',
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
           {
-            icon: 'pay-clock',
+            icon: 'pay-list',
             title: '可以半糖少冰嗎?',
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
           {
-            icon: 'pay-mobile',
+            icon: 'pay-clock',
             title: '還等到天荒地老，海枯石爛嗎?',
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
           {
-            icon: 'pay-mobile',
+            icon: 'pay-creditcard',
             title: '多元支付，比便利商店還方便！',
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
           {
-            icon: 'pay-qrcode',
+            icon: 'pay-cheers',
             title: '自己喝還是請客，掃碼一次搞定',
             text: '透過品牌專屬APP，消費者可以隨時下單，隨處購買，今天的我無極限'
           },
@@ -157,14 +159,14 @@
   .downhand {
     left: 250px;
   }
-  .magicBg{
+  .magicBg {
     height: 1600px;
     top: 50%;
     transform: translateY(-50%);
-    background-image: url(../assets/gradientBg.png),url(../assets/magicBg.png);
+    background-image: url(../assets/gradientBg.png), url(../assets/magicBg.png);
     background-repeat: no-repeat;
     background-position: center;
-    background-size: auto,cover;
+    background-size: auto, cover;
   }
   @media (max-width: 1440px) {
     .drinkCup {
--- src/views/newAccount.vue
+++ src/views/newAccount.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="account" class="">
+  <div id="account" class="text-main-500">
+    <popup
+      :class="{ hidden: popupHidden }"
+      title="帳號建立成功"
+      content="帳號建立完成，您可以登入會員中心囉。"
+      nextPage="/"
+    />
     <accountLogo />
     <div class="accountContainer m-auto block text-center px-10">
       <div class="entyForm pt-14">
@@ -16,7 +22,24 @@
             <input class="inputError" type="password" placeholder="請再次輸入密碼" />
             <span class="error text-left w-full inline-block">* 密碼不相符，請重新輸入</span>
           </div>
-          <router-link to="/verification" class="btn-dark-blue block">繼續</router-link>
+          <div class="inputBar">
+            <div class="flex">
+              <select name="" id="" v-model="selectedCode" class="text-sm text-sup1-500 w-3/5 mr-3">
+                <option :value="m.dial_code" v-for="(m, index) in mobileCode">
+                  {{ m.code }} {{ m.dial_code }}</option
+                >
+              </select>
+              <input class="border-white" type="text" placeholder="請輸入手機號碼" />
+            </div>
+            <div class="flex justify-between mt-3">
+              <span class="text-sm text-sup1-500">驗證碼已送出，請留意簡訊</span>
+              <div class="btn-light-blue flex-shrink-0 w-1/3 ml-4 duration-200">發送驗證碼</div>
+            </div>
+          </div>
+          <div class="inputBar">
+            <input type="text" placeholder="請輸入手機驗證碼" />
+          </div>
+          <div class="btn-dark-blue block mt-10" @click="popupHidden = false">建立帳號</div>
         </form>
       </div>
     </div>
@@ -24,18 +47,25 @@
   </div>
 </template>
 
-<script> 
+<script>
   import accountFooter from '@/components/accountFooter.vue'
   import accountLogo from '@/components/accountLogo.vue'
+  import mobileCode from '../../static/resource/CountryCodes'
+  import popup from '@/components/popup.vue'
 
   export default {
     name: 'newAccount',
     components: {
       accountFooter,
-      accountLogo
+      accountLogo,
+      popup
     },
     data() {
-      return {}
+      return {
+        selectedCode: '+886',
+        popupHidden: true,
+        mobileCode
+      }
     }
   }
 </script>
